cd64ee79192e28533eea5d51ea0d51d860dfcf63
ntevin25@gmail.com <ntevin25@gmail.com>
2023-10-17 09:44:57 +0200

    added sessions

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from models.user import User
 import getpass
 import sys
 import bcrypt
+import string
+import secrets
+import json
+import datetime
+
 
 # Database instance
 database = Database()
@@ -23,6 +28,33 @@ database.create_table("users", database.columns)
 # database.insert_into("USERS", column_names=[Id.key, Username.primary, Password.key], column_values=[
 #                      user_1.id, user_1.username, user_1.password])
 
+session_data = {}
+
+
+def generate_session_id():
+    alphabet = string.ascii_letters + string.digits
+    return "".join(secrets.choice(alphabet) for _ in range(32))
+
+
+def load_sessions():
+    try:
+        with open("session.json", "r") as file:
+            data = json.load(file)
+            session_data.update(data)
+    except Exception as e:
+        print(e)
+
+
+def save_session():
+    generate_id = generate_session_id()
+
+    try:
+        with open("session.json", "w") as session_store:
+            json.dump(session_data, session_store)
+
+    except Exception as e:
+        print(e)
+
 
 def sign_up():
     username = input("Enter username: ")
@@ -42,6 +74,10 @@ def sign_in():
                 prompt="Enter password: ", stream=sys.stderr)
             pw = password.encode("utf-8")
             if bcrypt.checkpw(pw, res[0][-1].encode("utf-8")):
+                data = {
+                    "user_id": res[0][0], "time": str(datetime.datetime.now())}
+                session_data[generate_session_id()] = data
+                save_session()
                 print("Successfully logged in!")
             else:
                 print("Incorrect username or password")
@@ -53,4 +89,5 @@ def sign_in():
 
 if __name__ == "__main__":
     sign_in()
+
     # sign_up()
